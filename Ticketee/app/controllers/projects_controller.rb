bad63da04276db9e55a89749b49cc7c3dc33ed5a
gamba14 <gambirassi.a@gmail.com>
2017-01-06 16:12:22 -0300

    CRUD LISTO

--- Ticketee/app/controllers/projects_controller.rb
+++ Ticketee/app/controllers/projects_controller.rb
@@ -41,6 +41,16 @@ class ProjectsController < ApplicationController
 
 	end
 
+	def destroy
+
+		@project = Project.find(params[:id])
+		@project.destroy
+
+		flash[:notice] = "Project has been deleted."
+		redirect_to projects_path
+
+	end
+
 	private
 
 	def project_params
