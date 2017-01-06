90f7e97d37a817c4d1920c9f9d33bb3477648e86
gamba14 <gambirassi.a@gmail.com>
2017-01-06 16:37:26 -0300

    Si los usuarios se mandan alguna te trae de vuelta al index

--- Ticketee/app/controllers/projects_controller.rb
+++ Ticketee/app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 	end
@@ -55,5 +58,12 @@ class ProjectsController < ApplicationController
 
 	def project_params
 		params.require(:project) .permit(:name, :description)
+	end
+
+	def set_project
+		@project = Project.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for could not be found."
+		redirect_to projects_path
 	end
 end
