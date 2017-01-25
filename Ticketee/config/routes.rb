b463452385f8a000d9f4c2f1a8151f6b0ef8767b
gamba14 <gambirassi.a@gmail.com>
2017-01-24 23:46:07 -0300

    agregado namespace admin con su controlador

--- Ticketee/config/routes.rb
+++ Ticketee/config/routes.rb
@@ -1,4 +1,8 @@
 Ticketee::Application.routes.draw do
+  namespace :admin do
+    root "application#index"
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "projects#index"
